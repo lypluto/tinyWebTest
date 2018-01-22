1ef99e2431cae515bde3706c536e277a3cf666ec
louis lu <louis.lu212@gmail.com>
2018-01-22 14:23:22 -0800

    cvv submit confirm

--- cvvTest/cvv.js
+++ cvvTest/cvv.js
@@ -7,7 +7,9 @@
 function onAddCvvClicked() {
     console.log("onAddCvvClicked()...");
     console.log("CVV submitted");
-    alert('CVV submitted');
+    //alert('CVV submitted');
+    //document.getElementById('answer1').value = "CVV submitted";
+    document.writeln("CVV submitted");
 }
 
 function onClearCvvClicked() {
